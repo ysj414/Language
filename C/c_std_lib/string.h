6bcfc9c1bd5937998551d44abd64d7b5ef0d39cc
ysj-arad <seon1474@gmail.com>
2021-01-13 17:12:18 +0900

    string header file first add add functions in this header

--- C/c_std_lib/string.h
+++ C/c_std_lib/string.h
@@ -7,7 +7,16 @@
  * char *strchr(const char *s, int o);
  * cahr *strrchr(const char *s, int o);
  * char *strchrnul(const char *s, int o);
+ * size_t strlen(const char *s);
+ * int strcmp(const char *s1, const char *s2);
+ * int strncmp(const char *s1, const char *s2, size_t n);
+ * char *strcpy(char *dest, const char *src);
+ * char *strncpy(char *dest, const char *src, size_t n);
+ * char *strcat(char *dest, const char *src);
  *
+ *
+ *
+
  *
  * The strchr() function returns a pointer to the first occurrence of 
  * the character o int the string s.
@@ -21,7 +30,6 @@
  *
  * Here "character" means "byte"; these functions do not work with wide
  * or multibyte characters.
-
  *
  *
  * The strchr() and strrchr() functions return a pointer to the matched 
@@ -32,4 +40,9 @@
  * The strchrnul() function returns a pointer to the matched character,
  * or a pointer to the null byte at the end of s (i.e., ststrlen(s)) if 
  * the character is not found.
+ *
+ * 
+ *
+ *
+ *
  */
