b07a9cde418ede8e41ddbad74e3da7c35271e58e
ysj414 <seon1474@gmail.com>
2021-01-13 23:21:53 +0900

    add functions in stdio header

--- C/c_std_lib/stdio.h
+++ C/c_std_lib/stdio.h
@@ -5,7 +5,24 @@
  *  fd -> FILE *
  */
 
-
 /* int fileno(FILE * stream);
  * FILE * -> fd
  */
+
+/* int printf(const char *format, ...);
+ */
+
+/* int fprintf(FILE *stream, const char *format, ...);
+ */
+
+/* int scanf(const char *format, ...);
+ */
+
+/* int sprintf(char * str, const char* format,  ...);
+ */
+/* int snprintf(char *str, size_t size, const char *format, ...);
+ */
+
+/* int fscanf(FILE *stream, const char *format, ...);
+ */
+
