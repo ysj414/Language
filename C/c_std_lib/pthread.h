23c5109cdd3cb908aeb33897feb1cc6f058ac123
ysj-arad <seon1474@gmail.com>
2021-01-13 17:14:15 +0900

    pthread header first add add functions in this header

---
+++ C/c_std_lib/pthread.h
@@ -0,0 +1,17 @@
+#include<pthread.h>
+
+/* int pthread_create(pthead_t *restrict thread, const pthread_attr_t *restrict attr,
+ *  void *(*start_routine)(void*), void *restrict arg);
+ */
+ 
+/* int pthread_join(pthread_t thread, void **status);
+ *
+ */
+
+
+
+// critical section
+/* Thread-safe function
+ * Thread-unsafe function
+ */
+
