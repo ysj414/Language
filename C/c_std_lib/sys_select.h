c321917f43e09fe3aacdfedac7c7a3b53e04eedc
ysj-arad <seon1474@gmail.com>
2021-01-13 17:17:50 +0900

    sys/select header first add add function in this header

---
+++ C/c_std_lib/sys_select.h
@@ -0,0 +1,8 @@
+#include<sys/select.h>
+
+
+
+/*
+ *  int select(int maxfd, fd_set *readset, fd_set *writeset, fd_set *exceptset, const struct timeval *timeout);
+ *  maxfd : the number of fd to check
+ */
