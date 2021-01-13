916b6fd0146df94f5eac8d6e9e332eca2960aefa
ysj-arad <seon1474@gmail.com>
2021-01-13 17:18:26 +0900

    sys/uio header file first add add functions in this header

---
+++ C/c_std_lib/sys_uio.h
@@ -0,0 +1,9 @@
+#include<sys/uio.h>
+
+/* ssize_t writev(int filedes, const struct iovec * iov, int iovcnt);
+ *
+ */
+
+/* ssize_t readv(int filedes, const struct iovec * iov, int iovcnt);
+ *
+ */
