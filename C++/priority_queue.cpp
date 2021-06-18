91d2cb2327c9f4c281162994bb374a62b3eb8697
ysj-arad <seon1474@gmail.com>
2021-06-18 17:47:33 +0900

    priority queue STL method

---
+++ C++/priority_queue.cpp
@@ -0,0 +1,26 @@
+#include <queue>
+
+using namespace std;
+
+
+
+priority_queue<int> q;
+priority_queue<int, vector<int>, less<int>> q1;
+priority_queue<int, vector<int>, greater<int>> q2;
+
+/* q.push(value)
+   top = q.top();
+   size = q.size();
+   emtpy = q.empty();
+   q.pop();
+ */
+
+int main(void)
+{
+
+
+
+
+
+	return 0;
+}
